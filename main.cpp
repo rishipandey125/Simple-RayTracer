2fe055c9eb500a4875b0de8f157ddad20932b897
rishipandey125 <35504258+rishipandey125@users.noreply.github.com>
2019-09-25 13:11:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "RayTracer.cpp"
 using namespace std;
 
-//features: multiple spheres, height&&depth, shadows, casted shadows
 int main() {
   vector<int> dimensions{1000,1500,-1500}; //formated as {height,width,depth}
   //colors of spheres
@@ -20,13 +19,6 @@ int main() {
   vector <int> green {0,230,77};
   //center points of spheres (cartesian coordiantes)
   Point p1(750,0,-200);
-  Point front1(600,0,-500);
-  Point front2(900,0,-500);
-  Point left(400,100,-700);
-  Point center(750,100,-700);
-  Point right(1100,100,-700);
-  Point left2(600,0,-900);
-  Point right2(900,0,-900);
   Point p2(300,0,-900);
   Point p3(1200,0,-900);
   Point p4(1000,100,-400);
@@ -38,20 +30,13 @@ int main() {
   Point l(750,800,400);
   //sphere objects
   Sphere first(150,p1,purple);
-  Sphere s_f1(150,front1,blue);
-  Sphere s_f2(150,front2,blue);
-  Sphere s_l(100,left,yellow);
-  Sphere s_c(200,center,green);
-  Sphere s_r(100,right,yellow);
-  Sphere s_l2(200,left2,red);
-  Sphere s_r2(200,right2,red);
   Sphere s_c3(200,p2,orange);
   Sphere last(200,p3,orange);
   Sphere s_p4(100,p4,teal);
   Sphere s_p5(100,p5,teal);
 
   //all sphere objects in the scene
-  std::vector <Sphere> objects {first,s_p4,s_p5,s_f1,s_f2,s_l,s_c,s_r,s_c3,s_l2,s_r2,last};
+  std::vector <Sphere> objects {first,s_c3,last,s_p4,s_p5};
   //all light points in the scene
   std::vector <Point> lights{l};
   render_image(dimensions, objects, lights);
